801ebd9accdf66d0d7adddf90c9e6462fc270a95
Ayres Narita <eu@ayresnarita.com>
2024-03-14 11:00:06 -0300

    first

---
+++ lib/toolbox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "toolbox/version"
+
+module Toolbox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/toolbox/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Toolbox
+  VERSION = "0.1.0"
+end
