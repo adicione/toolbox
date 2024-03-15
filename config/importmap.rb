9125357ff76fe1156defdd7ffe7abd37223b4394
Ayres Narita <eu@ayresnarita.com>
2024-03-15 10:42:39 -0300

    errors

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# pin "toolbox/hello_toolbox_controller", to: File.expand_path("../app/assets/javascripts/toolbox_controllers/hello_toolbox_controller.js", __FILE__)
+
+#pin "toolbox_controllers", to: "javascripts/hello_toolbox_controller.js"
+
+#pin_all_from File.expand_path("../app/assets/javascripts", __dir__)
--- lib/toolbox.rb
+++ lib/toolbox.rb
@@ -3,6 +3,13 @@
 require_relative "toolbox/version"
 
 module Toolbox
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError
+  end
+
+  class Engine < ::Rails::Engine
+    initializer "toolbox.importmap", before: "importmap" do |app|
+      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+      app.config.importmap.cache_sweepers << Engine.root.join("app/assets/javascripts/toolbox")
+    end
+  end
 end
