713256e10851efdcd553c2793477e714fbbd5c1d
Igor Estevao <igor.1999.gigs@gmail.com>
2026-01-08 16:38:10 -0300

    Update authorization handling in HelpersController

--- app/controllers/toolbox/helpers_controller.rb
+++ app/controllers/toolbox/helpers_controller.rb
@@ -1,6 +1,8 @@
 module Toolbox
   class HelpersController < ApplicationController
-    skip_after_action :verify_authorized if defined?(Pundit) # Pundit policy check.
+    # skip_after_action :verify_authorized if defined?(Pundit) # Pundit policy check.
+    skip_authorization
+    skip_policy_scope
 
     def confirmation
       @destination = params[:destination] || "#"
